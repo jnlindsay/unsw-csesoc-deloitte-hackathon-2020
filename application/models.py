5503a55481398c36fbc1ad3b9adab17a6b25c6fd
Adam Ma <adamma@Adam-Ma.local>
2020-06-12 22:08:16 +1000

    data done

--- application/models.py
+++ application/models.py
@@ -13,12 +13,12 @@ class Task(db.Model):
 class Covid(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
-    location = db.Column(db.String(200), nullable=False)
-    country_id = db.Column(db.Integer, db.ForeignKey('country.id'),
-        nullable=False)
+    num_cases = db.Column(db.Integer, default=0, nullable=False)
+    #location = db.Column(db.String(200), nullable=True)
+    country_id = db.Column(db.Integer, db.ForeignKey('country.id'), nullable=False)
     # 0 for not 1 yes
     def __repe__(self):
-        return '<Covid%r>' % self.id
+        return '<id %r>' % self.id
 
 
 class Country(db.Model):
@@ -26,5 +26,5 @@ class Country(db.Model):
     name = db.Column(db.String(200), nullable=False)
     Covid = db.relationship('Covid', backref='country', lazy=True)
     def __repe__(self):
-        return '<Country%r>' % self.id
+        return '<country_id %r>' % self.id
 
