5ea160f21c22687c91527b0adcd2df60f1d7ea40
Adam Ma <adamma@Adam-Ma.local>
2020-06-12 18:15:31 +1000

    adding data model

--- application/models.py
+++ application/models.py
@@ -1,6 +1,16 @@
 from datetime import datetime
 from application import db
 
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+    location = db.Column(db.String(200), nullable=False)
+    # 0 for not 1 yes
+    def __repe__(self):
+        return '<Covid New Cases%r>' % self.id
+
+
+
 class Covid(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
