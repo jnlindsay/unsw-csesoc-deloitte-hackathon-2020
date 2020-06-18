24e521b086822b1c6e62af3ce90100139d0af220
adammbq <adammbq@gmail.com>
2020-06-18 16:45:38 +1000

    Cleaned up

--- application/models.py
+++ application/models.py
@@ -1,25 +1,14 @@
 from datetime import datetime
 from application import db
 
-class Task(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    date_created = db.Column(db.DateTime, nullable=False)
-    location = db.Column(db.String(200), nullable=False)
-    # 0 for not 1 yes
-    def __repe__(self):
-        return '<Covid New Cases%r>' % self.id
-
-
 class Covid(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
     num_cases = db.Column(db.Integer, default=0, nullable=False)
     #location = db.Column(db.String(200), nullable=True)
     suburb_id = db.Column(db.Integer, db.ForeignKey('suburb.id'), nullable=False)
-    # 0 for not 1 yes
     def __repe__(self):
         return '<id %r>' % self.id
-
 
 class Suburb(db.Model):
     id = db.Column(db.Integer, primary_key=True)
--- application/routes.py
+++ application/routes.py
@@ -3,13 +3,13 @@ from flask import request, render_template, url_for, redirect, json
 
 ''' Import functions'''
 from application import app
-from application.models import Task, Covid, Suburb
-from application.functions.task_funcs import tasks
+from application.models import Covid, Suburb
+
 
 from application import db
 
 #######################################################
-#                  HEALTH RISKS                       #
+#                 HealthView Routes                   #
 #######################################################
 
 @app.route('/search')
@@ -38,12 +38,6 @@ def home():
         all_data = Covid.query.filter_by(suburb_id=suburb_data.id).order_by(Covid.date_created).all()
         # Get most recent data
         recent_data = all_data[:14]
-
-        #turn data into day/month 
-
-
-
-
         # calculate sum of recent data
         sum = 0
         dates = []
@@ -60,31 +54,6 @@ def home():
         }
         rate = "{0:.4%}".format(sum / population['bondi'])
         return render_template('home.html', sum=sum, suburb_name=suburb.title(), empty=1, labels=dates, data=data, rate=rate)
-        '''
-
-        print("Redirected with the following"
-                "request data: " + request.form['suburb'])
-        try:
-            return redirect('/')
-        except:
-            return "There was an issue with the POST request."
-        '''
-
-@app.route('/suburb/australia')
-def suburb_australia():
-    return render_template('suburb_australia.html')
-
-@app.route('/suburb/brazil')    
-def suburb_brazil():
-    return render_template('suburb_brazil.html')
-
-@app.route('/city/sydney')    
-def city_sydney():
-    return render_template('city_sydney.html')
-
-@app.route('/city/sao-paulo')    
-def city_sao_paulo():
-    return render_template('city_sao-paulo.html')
 
 
 @app.route('/admin', methods=['POST', 'GET'])
@@ -96,13 +65,19 @@ def admin():
         num_cases = data.get('num_cases')
         # find suburb in database and get suburb id
         suburb_data = Suburb.query.filter_by(name=suburb).all()
+
+        '''
+        Error Checking 
+        '''
         if suburb_data is None:
             return "invalid suburb or suburb doesnt exist"
         try:
             suburb_id = suburb_data[0].id
         except IndexError:
             return "invalid suburb or suburb doesnt exist"
-
+        '''
+        Turn string into datetime object
+        '''
         date_created = datetime.strptime(date_str, '%Y-%m-%d').date()
         new_covid = Covid(suburb_id=suburb_id, date_created=date_created, num_cases=num_cases)
         try:
@@ -114,8 +89,6 @@ def admin():
 
             return "There was an issue assing your task"
     else:
-        print("get")
-
         covid = Covid.query.filter_by().all()
         suburb = Suburb.query.filter_by().all()
         print(covid)
@@ -139,78 +112,7 @@ def suburb():
             db.session.commit()
             return redirect('/suburb')
         except:
-            print(new_covid.suburb_id, date_created, num_cases)
             return "There was an issue assing your task"
     else:
-        print("get")
         suburb = Suburb.query.filter_by().all()
         return render_template('suburb.html', suburb=suburb)
-
-
-#######################################################
-
-# @app.route('/task', methods=['POST', 'GET'])
-# def taskPage():
-#     if request.method == "POST":
-#         task_content = request.form['content']
-#         new_task = Task(content=task_content)
-#         print("post")
-#         try:
-#             db.session.add(new_task)
-#             db.session.commit()
-#             return redirect('/task')
-#         except:
-#             return "There was an issue assing your task"
-#     else:
-#         print("get")
-#         u_tasks = Task.query.filter_by(completed=0).all()
-#         c_tasks = Task.query.filter_by(completed=1).all()
-#         return render_template('task.html', u_tasks=u_tasks, c_tasks=c_tasks)
-
-# @app.route('/task/delete/<int:id>')
-# def delete(id):
-#     task_to_delete = Task.query.get_or_404(id)
-#     try:
-#         db.session.delete(task_to_delete)
-#         db.session.commit()
-#         return redirect('/task')
-#     except:
-#         return "There was a problem deleting that task"
-
-# @app.route('/task/update/<int:id>', methods=['GET', 'POST'])
-# def update(id):
-#     task_to_update = Task.query.get_or_404(id)
-#     if request.method == 'POST':
-#         task_to_update.content = request.form['content']
-#         try:
-#             db.session.commit()
-#             return redirect('/task')
-#         except:
-#             return "There was a problem deleting that task"
-#     else:
-#         return render_template('update.html', task=task_to_update)
-
-# @app.route('/task/complete/<int:id>')
-# def complete(id):
-#     task_to_complete = Task.query.get_or_404(id)
-#     task_to_complete.completed = 1
-#     try:
-#         db.session.commit()
-#         return redirect('/task')
-#     except:
-#         return "There was a problem completing that task"
-
-
-# @app.route('/task/uncomplete/<int:id>')
-# def uncomplete(id):
-#     task_to_complete = Task.query.get_or_404(id)
-#     task_to_complete.completed = 0
-#     try:
-#         db.session.commit()
-#         return redirect('/task')
-#     except:
-#         return "There was a problem completing that task"
-
-# @app.route('/about')
-# def about():
-#     return render_template('about.html')
